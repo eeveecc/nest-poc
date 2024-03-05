42797d7f771b6b931933c66f4c4cacebc27b01e5
eeveecc <cys0919@gmail.com>
2024-03-04 23:38:17 -0500

    add middlewares

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
 
-
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -12,11 +13,13 @@ export class EmployeesController {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @SkipThrottle({ default: false })
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     return this.employeesService.findAll();
   }
 
+  @Throttle({ short: { ttl: 1000, limit: 1 }}) // override the default
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  // app.enableCors(); // TODO: add local origins
   await app.listen(3000);
 }
 bootstrap();
